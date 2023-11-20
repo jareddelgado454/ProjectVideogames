8c7d94008e80684984f26ce64923fdd5bbb77ff6
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2023-11-19 19:30:39 -0500

    Last CHanges at night

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,16 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DATABASE_URL,
+  PGDATABASE,
+  PGHOST,
+  PGPASSWORD,
+  PGPORT,
+  PGUSER,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(DATABASE_URL || `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`, {
+  dialect: 'postgres',
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
